0a0467fd812fdbd11d67205dafd575420243a1e2
joaocarloslima <joaocarloslima@me.com>
2026-03-03 09:56:08 -0300

    add Movie model, controller, and service for movie management

--- src/main/java/br/com/fiap/movies/controllers/MovieController.java
+++ src/main/java/br/com/fiap/movies/controllers/MovieController.java
@@ -1,14 +1,40 @@
 package br.com.fiap.movies.controllers;
 
-import org.springframework.stereotype.Controller;
+import br.com.fiap.movies.models.Movie;
+import br.com.fiap.movies.services.MovieService;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.HttpStatus;
 import org.springframework.web.bind.annotation.*;
+
+import java.time.LocalDate;
+import java.util.List;
 
 @RestController
 public class MovieController {
 
+    private final Logger log = LoggerFactory.getLogger(getClass());
+
+    @Autowired
+    private MovieService service;
+
     @GetMapping("/")
     public String healthCheck() {
         return "Server UP";
+    }
+
+    @GetMapping("/movies")
+    public List<Movie> getMovies(){
+        log.info("Listando todos os filmes");
+        return service.getMovies();
+    }
+
+    @PostMapping("/movies")
+    @ResponseStatus(code = HttpStatus.CREATED)
+    public Movie addMovie(@RequestBody Movie movie){ //Binding do JSON para o objeto Movie
+        log.info("Cadastrando filme...");
+        return service.addMovie(movie);
     }
 
 }
---
+++ src/main/java/br/com/fiap/movies/models/Movie.java
@@ -0,0 +1,16 @@
+package br.com.fiap.movies.models;
+
+import lombok.AllArgsConstructor;
+import lombok.Data;
+
+import java.time.LocalDate;
+
+@Data
+@AllArgsConstructor
+public class Movie {
+    private String title;
+    private String synopsis;
+    private Integer rating;
+    private LocalDate releaseDate;
+
+}
